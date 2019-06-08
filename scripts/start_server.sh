dceb016ad4c0de07d47d4933e3833ed1c30b5a82
Camacho, Juan <juan.camacho@intel.com>
2019-06-08 15:03:12 -0600

    CB and CD files

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pm2 start java -jar target/myapp-1.0.jar --name "JAVAWebApp" --cwd /myapp/ -- start
+pm2 start java --name "JAVAWebApp" --cwd /myapp/ --args [-jar=target/myapp-1.0.jar] -- start
